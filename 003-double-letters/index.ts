ed3ee45deafdc53b3fca81084f7dac03012cccb4
Anton de Regt <antonderegt@pm.me>
2021-04-17 09:23:51 +0200

    Simplify solution with reduce

--- 003-double-letters/index.ts
+++ 003-double-letters/index.ts
@@ -1,28 +1,15 @@
 export function splitOnDoubleLetter(word:string) {
-    const splitOnEveryLetter = word.toLowerCase().split('');
-    let tempWord = splitOnEveryLetter[0];
-    let result = [];
-    let foundDoubleLetter = false;
-    
-    for(let i = 1; i < splitOnEveryLetter.length; i++) {
-        const currLetter = splitOnEveryLetter[i];
-        const prevLetter = splitOnEveryLetter[i - 1]
-
+    const result = word.split('').reduce((splitWords, currLetter) => {
+        const prevLetter = splitWords[splitWords.length - 1].slice(-1);
         if(currLetter === prevLetter) {
-            result.push(tempWord);
-            tempWord = currLetter;
-            foundDoubleLetter = true;
+            splitWords.push(currLetter);
         } else {
-            tempWord += currLetter;
+            splitWords[splitWords.length - 1] = splitWords[splitWords.length - 1] + currLetter;
         }
-    }
+        return splitWords;
+    }, [""])
 
-    if(!foundDoubleLetter) return [];
-
-    result.push(tempWord);
+    if(result.length === 1) return [];
 
     return result;
 }
-
-// Improve by using a slice from last found double letter. 
-// No need for splitting the word
