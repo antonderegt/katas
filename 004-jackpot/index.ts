b3b661d1ab6aa1834608b6c84b2ea3ab460abd87
Anton de Regt <antonderegt@pm.me>
2021-04-19 09:45:30 +0200

    Optimize solution to return on first mismatch

--- 004-jackpot/index.ts
+++ 004-jackpot/index.ts
@@ -1,12 +1,12 @@
 export function jackpot(elements:Array<string>) {
     if(elements === undefined || elements.length != 4) throw new Error('Please supply four elements.');
 
-    let elementsAreIdentical = true;
+    for(let i = 1; i < elements.length; i++) {
+        const prevString = elements[i - 1];
+        const currString = elements[i];
+        
+        if(prevString != currString) return false;
+    }
 
-    elements.reduce((prevString, currString) => {
-        if(prevString != currString) elementsAreIdentical = false;
-        return currString;
-    }, elements[0])
-
-    return elementsAreIdentical;
+    return true;
 }
