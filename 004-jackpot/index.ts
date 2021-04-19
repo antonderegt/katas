bffc1a80c1baa2a5286d25cd8f8f30e15d8d2d28
Anton de Regt <antonderegt@pm.me>
2021-04-19 10:09:43 +0200

    Add strict type checking

--- 004-jackpot/index.ts
+++ 004-jackpot/index.ts
@@ -4,8 +4,8 @@ export function jackpot(elements:Array<string>) {
     for(let i = 1; i < elements.length; i++) {
         const prevString = elements[i - 1];
         const currString = elements[i];
-        
-        if(prevString != currString) return false;
+
+        if(prevString !== currString) return false;
     }
 
     return true;
