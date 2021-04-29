96aa78f62984e6e0036c4b65b6dedf113609266c
Anton de Regt <antonderegt@pm.me>
2021-04-29 10:05:05 +0200

    Add test

--- 010-stranger-danger/index.test.ts
+++ 010-stranger-danger/index.test.ts
@@ -8,4 +8,8 @@ describe('010 Stranger Danger', () => {
     test('String returns [[spot, see],[]]', () => {
         expect(noStrangers("See Spot run. See Spot jump. Spot likes jumping. See Spot fly.")).toEqual([["spot", "see"], []])
     })
+
+    test('String returns [[see],[spot]]', () => {
+        expect(noStrangers("See Spot Spot spot run. See Spot jump. Spot likes jumping. See Spot fly.")).toEqual([["see"], ["spot"]])
+    })
 })
